5f80523b931056d5e0ad43e08e1094b0801fb5eb
Molodkin Viktor <vitamin_C2004@yahoo.com>
2024-07-11 23:31:51 +0300

    PP313 v1.1 (Security_bootstrap)

--- src/main/java/ru/kata/spring/boot_security/demo/configs/SuccessUserHandler.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/SuccessUserHandler.java
@@ -1,6 +1,5 @@
 package ru.kata.spring.boot_security.demo.configs;
 
-import antlr.collections.List;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -10,7 +9,6 @@ import ru.kata.spring.boot_security.demo.security.UserDetail;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Set;
 
 @Component
